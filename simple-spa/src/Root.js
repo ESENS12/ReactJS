b3ac6caaece15da07dcaaa1150c17d0307b21158
ESENS12 <ghdldid12@gmail.com>
2020-01-09 12:26:01 +0900

    Add Simple SPA

---
+++ simple-spa/src/Root.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import App from 'App';
+import {BrowserRouter} from 'react-router-dom';
+
+const Root = () => (
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
+export default Root;
